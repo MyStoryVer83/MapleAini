197946f7aabf63f94cd422fad05397872162bd25
Atoot <hjn_63022152@126.com>
2017-04-25 19:12:18 +0800

    4转任务BOSS地图
    4转任务BOSS地图

--- scripts/portal/gryphius.js
+++ scripts/portal/gryphius.js
@@ -2,7 +2,8 @@ function enter(pi) {
     if(pi.isQuestStarted(1451) || pi.isQuestStarted(1453) || pi.isQuestStarted(1455) || pi.isQuestStarted(1457) || pi.isQuestStarted(1459)){
         pi.resetMap(924000201);
         pi.warp(924000201, 2);
-    }else{
+    } else {
     pi.warp(240020101, "out00");
-}
+    }
+	return true;	
 }
