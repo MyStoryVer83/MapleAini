efe75e06d0b3b1b99ebf28cc5984737bb5ad9345
Atoot <hjn_63022152@126.com>
2017-04-25 19:59:53 +0800

    4转测试
    4转测试

--- scripts/portal/mayong.js
+++ scripts/portal/mayong.js
@@ -20,6 +20,10 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 function enter(pi) {
+    if(pi.isQuestStarted(6904) || pi.isQuestStarted(6914) || pi.isQuestStarted(6924) || pi.isQuestStarted(6934) || pi.isQuestStarted(6944)){
+        pi.resetMap(240020402);
+        pi.warp(240020402, 2);
+    } else {
 	pi.warp(240020401, "out00");
 	return true;
 }
